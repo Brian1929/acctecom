b33ea30cc4c513ec79f6699274ad20653c7825f2
Brii91 <britney.workmail91@gmail.com>
2026-05-25 09:37:51 -0400

    Remove nested .git and fix submodule issue

---
+++ acctecom/lib/contact.js
@@ -0,0 +1,9 @@
+/** Datos de contacto oficiales de AccTecom */
+export const ACCTECOM_PHONE_DISPLAY = '809-583-6872';
+export const ACCTECOM_PHONE_TEL = '8095836872';
+export const ACCTECOM_PHONE_WHATSAPP = '18095836872';
+export const ACCTECOM_EMAIL = 'acctecomrd@gmail.com';
+
+export const acctecomTelHref = `tel:${ACCTECOM_PHONE_TEL}`;
+export const acctecomMailHref = `mailto:${ACCTECOM_EMAIL}`;
+export const acctecomWhatsAppHref = `https://wa.me/${ACCTECOM_PHONE_WHATSAPP}`;
